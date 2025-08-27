87d9352d35a0c9b690f4aaaef9329eb84ecbce9c
japankoreya666-cell <japankoreya666@gmail.com>
2025-08-27 14:06:59 +0530

    Update shubh.py

--- shubh.py
+++ shubh.py
@@ -6,7 +6,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7906442376:AAFUu_ppRQZsiDy9EgYSyhTjLQY3zAalL28')
+bot = telebot.TeleBot('8495942259:AAFGn9I6ksNv_KOgbN0j96YlEdIpqbmxYjU')
 
 # Admin user IDs
 admin_id = ["1817896911"]
